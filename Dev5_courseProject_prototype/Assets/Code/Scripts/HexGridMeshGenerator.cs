0b87300fe7490c215280d14e61d6f9ce7552dbab
EphramJanssens <ephram.janssens@student.ehb.be>
2025-11-02 18:58:50 +0100

    Added mouse registration when clicking on hex fields

--- Dev5_courseProject_prototype/Assets/Code/Scripts/HexGridMeshGenerator.cs
+++ Dev5_courseProject_prototype/Assets/Code/Scripts/HexGridMeshGenerator.cs
@@ -1,6 +1,7 @@
 using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer), typeof(MeshCollider))]
 public class HexGridMeshGenerator : MonoBehaviour
@@ -8,6 +9,7 @@ public class HexGridMeshGenerator : MonoBehaviour
     [field: SerializeField] public LayerMask gridLayer { get; private set; }
     [field: SerializeField] public HexGrid hexGrid { get; private set; }
     [SerializeField] private Material defaultMaterial;
+    [SerializeField] private GameObject explosionTest;
 
 
     private void Awake()
@@ -16,7 +18,19 @@ public class HexGridMeshGenerator : MonoBehaviour
             hexGrid = GetComponentInParent<HexGrid>();
         if (hexGrid == null)
             Debug.LogError("HexGridMeshGenerator could not find a HexGrid component in its parent or itself.");
-        }
+    }
+
+    private void OnEnable()
+    {
+        MouseController.instance.OnLeftMouseClick += OnLeftMouseClick;
+        MouseController.instance.OnRightMouseClick += OnRightMouseClick;
+    }
+
+    private void OnDisable()
+    {
+        MouseController.instance.OnLeftMouseClick -= OnLeftMouseClick;
+        MouseController.instance.OnRightMouseClick -= OnRightMouseClick;
+    }
 
     public void CreateHexMesh()
     {
@@ -110,5 +124,25 @@ public class HexGridMeshGenerator : MonoBehaviour
             }
         }
         return 0;
+    }
+
+    private void OnLeftMouseClick(RaycastHit hit)
+    {
+        Debug.Log("Hit object:" + hit.transform.name + "at position" + hit.point);
+        float localX = hit.point.x - hit.transform.position.x;
+        float localZ = hit.point.z - hit.transform.position.z;
+        //Debug.Log("Hex position:" + HexMetrics.CoordinateToAxial(localX, localZ, Grid.Hexsize, Grid.Orientation));
+        Debug.Log("Offset Position:" + HexMetrics.CoordinateToOffset(localX, localZ, hexGrid.HexSize, hexGrid.Orientation));
+    }
+
+    private void OnRightMouseClick(RaycastHit hit)
+    {
+        float localX = hit.point.x - hit.transform.position.x;
+        float localZ = hit.point.z - hit.transform.position.z;
+
+        Vector2 location = HexMetrics.CoordinateToOffset(localX, localZ, hexGrid.HexSize, hexGrid.Orientation);
+        Vector3 center = HexMetrics.Center(hexGrid.HexSize, (int)location.x, (int)location.y, hexGrid.Orientation);
+        Debug.Log("Right Clicked on Hex:" + location);
+        Instantiate(explosionTest, center, Quaternion.identity);
     }
 }
--- Dev5_courseProject_prototype/Assets/Code/Scripts/HexMetrics.cs
+++ Dev5_courseProject_prototype/Assets/Code/Scripts/HexMetrics.cs
@@ -1,3 +1,5 @@
+using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
@@ -52,64 +54,144 @@ public static class HexMetrics
         }
         return centerPosition;
     }
-    
-     public static Vector3 OffsetToCube(int col, int row, HexOrientation orientation)
 
+    public static Vector2 CubeToAxial(Vector3 cube)
+    {
+        return new Vector2(cube.x, cube.y);
+    }
+
+    public static Vector2 OffsetToAxial(int x, int z, HexOrientation orientation)
+
+    {
+
+        if (orientation == HexOrientation.PointyTop)
+        {
+            return OffsetToAxialPointy(x, z);
+        }
+        else
+        {
+            return OffsetToAxialFlat(x, z);
+        }
+    }
+
+    public static Vector2Int OffsetToAxialFlat(int col, int row)
   {
-
-      if (orientation == HexOrientation.PointyTop)
-
-      {
-
-          return AxialToCube(OffsetToAxialPointy(col, row));
-
-      }
-
-      else
-
-      {
-
-          return AxialToCube(OffsetToAxialFlat(col, row));
-
-      }
-
-  }
-
-  public static Vector3 AxialToCube(Vector2Int axial)
-
-  {
-
-      float x = axial.x;
-
-      float z = axial.y;
-
-      float y = -x - z;
-
-      return new Vector3(x, z, y);
-
-  }
-
-  public static Vector2Int OffsetToAxialFlat(int col, int row)
-
-  {
-
       int q = col;
-
       int r = row - (col + (col & 1)) / 2;
-
       return new Vector2Int(q, r);
-
   }
+    public static Vector2Int OffsetToAxialPointy(int col, int row)
+    {
+        int q = col - (row + (row & 1)) / 2;
+        int r = row;
+        return new Vector2Int(q, r);
+    }
 
-  public static Vector2Int OffsetToAxialPointy(int col, int row)
+    public static Vector2 CubeToOffset(int x, int y, int z, HexOrientation orientation)
+    {
+        if (orientation == HexOrientation.PointyTop)
+        {
+            return CubeToOffsetPointy(x, y, x);
+        }
+        else
+        {
+            return CubeToOffsetFlat(x, y, z);
+        }
+    }
 
-  {
+    public static Vector2 CubeToOffset(Vector3 offsetCoord, HexOrientation orientation)
+    {
+        return CubeToOffset((int)offsetCoord.x, (int)offsetCoord.y, (int)offsetCoord.z, orientation);
+    }
 
-      int q = col - (row + (row & 1)) / 2;
+    private static Vector2 CubeToOffsetPointy(int x, int y, int z)
+    {
+        Vector2 offsetCoordinates = new Vector2(x + (y - (y & 1)) / 2, y);
+        return offsetCoordinates;
+    }
 
-      int r = row;
+    private static Vector2 CubeToOffsetFlat(int x, int y, int z)
+    {
+        Vector2 offsetCoordinates = new Vector2(x, y + (x - (x & 1)) / 2);
+        return offsetCoordinates;
+    }
 
-      return new Vector2Int(q, r);
+    private static Vector3 CubeRound(Vector3 frac)
+    {
+        Vector3 roundedCoordinates = new Vector3();
+        int rx = Mathf.RoundToInt(frac.x);
+        int ry = Mathf.RoundToInt(frac.y);
+        int rz = Mathf.RoundToInt(frac.z);
+        float xDiff = Mathf.Abs(rx - frac.x);
+        float yDiff = Mathf.Abs(ry - frac.y);
+        float zDiff = Mathf.Abs(rz - frac.z);
 
-  }
+        if (xDiff > yDiff && xDiff > zDiff)
+        {
+            rx = -ry - rz;
+        }
+        else if (yDiff > zDiff)
+        {
+            ry = -rx - rz;
+        }
+        else
+        {
+            rz = -rx - ry;
+        }
+        roundedCoordinates.x = rx;
+        roundedCoordinates.y = ry;
+        roundedCoordinates.z = rz;
+        return roundedCoordinates;
+    }
+
+    public static Vector2 AxialRound(Vector2 coordinates)
+    {
+        return CubeToAxial(CubeRound(AxialToCube(coordinates.x, coordinates.y)));
+    }
+
+    public static Vector2 CoordinateToAxial(float x, float z, float Hexsize, HexOrientation orientation)
+    {
+        if (orientation == HexOrientation.PointyTop)
+        {
+            return CoordinateToPointyAxial(x, z, Hexsize);
+        }
+        else
+        {
+            return CoordinateToFlatAxial(x, z, Hexsize);
+        }
+    }
+
+    private static Vector2 CoordinateToPointyAxial(float x, float z, float Hexsize)
+    {
+        Vector2 pointyHexCoordinates = new Vector2();
+        pointyHexCoordinates.x = (2f / 3 * x) / Hexsize;
+        pointyHexCoordinates.y = (-1f / 3 * x + Mathf.Sqrt(3) / 3 * z) / Hexsize;
+        return AxialRound(pointyHexCoordinates);
+    }
+
+    public static Vector2 CoordinateToFlatAxial(float x, float z, float HexSize)
+    {
+        Vector2 flatHexCoordinates = new Vector2();
+        flatHexCoordinates.x = (Mathf.Sqrt(3) / 3 * x - 1f / 3 * z) / HexSize;
+        flatHexCoordinates.y = (2f / 3 * z) / HexSize;
+        return AxialRound(flatHexCoordinates);
+    }
+
+    public static Vector2 CoordinateToOffset(float x, float z, float Hexsize, HexOrientation orientation)
+    {
+        return CubeToOffset(AxialToCube(CoordinateToAxial(x, z, Hexsize, orientation)), orientation);
+    }
+
+    public static Vector3 AxialToCube(int q, int r)
+    {
+        return new Vector3(q, r, -q - r);
+    }
+    public static Vector3 AxialToCube(float q, float r)
+    {
+        return new Vector3(q, r, -q - r);
+    }
+    public static Vector3 AxialToCube(Vector2 axialCoord)
+    {
+        return AxialToCube(axialCoord.x, axialCoord.y);
+    }
 }
