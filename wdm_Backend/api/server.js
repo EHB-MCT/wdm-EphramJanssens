075e233c60de2e4120d26493b51088750e1b2196
EphramJanssens <ephram.janssens@student.ehb.be>
2026-01-02 13:09:39 +0100

    chore: Cleaned up code to make it read better

--- wdm_Backend/api/server.js
+++ wdm_Backend/api/server.js
@@ -5,30 +5,28 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// --- Middleware ---
-app.use(cors()); // Sta Cross-Origin requests toe (voor Unity)
-app.use(express.json()); // Parse binnenkomende JSON-data
+app.use(cors()); 
+app.use(express.json()); 
 
-// --- Database Connectie ---
+
 const pool = new Pool({
     user: process.env.DB_USER,
-    // BELANGRIJK: 'db' is de servicenaam in docker-compose.yml
     host: process.env.DB_HOST, 
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: 5432,
 });
 
-// --- Endpoints ---
+
 app.get('/', (req, res) => {
     res.send('Backend API draait en is klaar om data te ontvangen!');
 });
 
-// Dit is het endpoint waar je Unity-game naartoe stuurt
+
 app.post('/api/log', async (req, res) => {
     const { userId, actionType, payload } = req.body;
 
-    // Requirement: "Data is cleaned, or otherwise checked"
+
     if (!userId || !actionType) {
         return res.status(400).send({ error: 'Missing required fields: userId and actionType' });
     }
@@ -36,12 +34,11 @@ app.post('/api/log', async (req, res) => {
     console.log(`Data ontvangen van user ${userId}: ${actionType}`);
 
     try {
-        // Sla de data op in de database
         const query = 'INSERT INTO game_logs (user_id, action_type, payload) VALUES ($1, $2, $3) RETURNING *';
-        const values = [userId, actionType, payload || {}]; // Stuur {} als payload null is
+        const values = [userId, actionType, payload || {}];
 
         const result = await pool.query(query, values);
-        res.status(201).send(result.rows[0]); // Stuur de gemaakte log terug
+        res.status(201).send(result.rows[0]);
 
     } catch (err) {
         console.error('Error met database:', err);
@@ -49,7 +46,16 @@ app.post('/api/log', async (req, res) => {
     }
 });
 
-// --- Start Server ---
+app.get ("/api/logs", async (req, res) => {
+    try{
+        const result = await pool.query("SELECT * FROM game_logs ORDER BY created_at DESC LIMIT 100");
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({error: "Database error when fetching logs"});
+    }
+});
+
 app.listen(port, () => {
     console.log(`API server luistert op http://localhost:${port}`);
 });
